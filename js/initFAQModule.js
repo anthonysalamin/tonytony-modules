06959d020a35d6fd3b091216c4e28500b7e60aff
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2025-09-22 18:57:36 +0800

    remocved dom

--- js/initFAQModule.js
+++ js/initFAQModule.js
@@ -4,41 +4,26 @@
  */
 
 export function initFAQModule() {
-    document.addEventListener("DOMContentLoaded", initFAQ);
+    const questions = document.querySelectorAll(".faq__question");
+    if (questions.length === 0) return;
 
-    function initFAQ() {
-        const questions = document.querySelectorAll(".faq__question");
-        if (questions.length === 0) return;
+    gsap.set(".faq__answer", { height: 0, overflow: "hidden", duration: 0 });
 
-        gsap.set(".faq__answer", { height: 0, overflow: "hidden", duration: 0 });
+    function openFirstFAQ() { questions[0].click(); }
+    setTimeout(openFirstFAQ, 250);
 
-        function openFirstFAQ() {
-            questions[0].click();
-        }
-
-        setTimeout(openFirstFAQ, 250);
-
-        questions.forEach((question) => {
-            let isOpen = false;
-
-            question.addEventListener("click", () => {
-                const icon = question.querySelector(`.icon-plus`);
-                const answer = question.nextElementSibling;
-
-                if (isOpen) {
-                    collapseAnswer(answer, icon);
-                } else {
-                    expandAnswer(answer, icon);
-                }
-
-                isOpen = !isOpen;
-            });
+    questions.forEach(question => {
+        let isOpen = false;
+        question.addEventListener("click", () => {
+            const icon = question.querySelector(".icon-plus");
+            const answer = question.nextElementSibling;
+            isOpen ? collapseAnswer(answer, icon) : expandAnswer(answer, icon);
+            isOpen = !isOpen;
         });
-    }
+    });
 
     function expandAnswer(answer, icon) {
         gsap.set(answer, { height: "auto" });
-        const height = answer.clientHeight;
         gsap.from(answer, { height: 0, duration: 0.3, ease: "ease-in" });
         gsap.to(icon, { rotationZ: 45, duration: 0.3, ease: "ease-out" });
     }
