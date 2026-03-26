313dd1e78183db06cd319648e65e34635be7dbc2
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-03-26 11:31:44 +0800

    Update initFathomTrackEvents.js

--- js/initFathomTrackEvents.js
+++ js/initFathomTrackEvents.js
@@ -4,22 +4,23 @@
  * @build 26.03.26
  * @updated 18:02 CET
  */
+
 export function initFathomTrackEvent() {
-    window.addEventListener('load', () => {
-      // Button tracking
-      const btn = document.querySelector('[data-trackevent="btn-free-seo-audit"]');
-      if (btn) {
-        btn.addEventListener('click', () => {
-          fathom.trackEvent('home contact click');
-        });
-      }
-  
-      // Form tracking
-      const form = document.getElementById('id-of-your-form');
-      if (form) {
-        form.addEventListener('submit', () => {
-          fathom.trackEvent('form submit');
-        });
-      }
-    });
-  }
+  window.addEventListener('load', () => {
+    // Button tracking
+    const btn = document.querySelector('[data-trackevent="btn-free-seo-audit"]');
+    if (btn) {
+      btn.addEventListener('click', () => {
+        fathom.trackEvent('home contact click');
+      });
+    }
+
+    // Form tracking
+    const form = document.getElementById('contact-form');
+    if (form) {
+      form.addEventListener('submit', () => {
+        fathom.trackEvent('contact form submit');
+      });
+    }
+  });
+}
