0754d25747b70cf9e36966cd20c746f3f3ab3915
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-04-07 17:37:51 +0800

    Update initSwitchTheme.js

--- js/initSwitchTheme.js
+++ js/initSwitchTheme.js
@@ -1,6 +1,8 @@
 /**
  * TONYTONY | initSwitchTheme
+ * *
  * Toggles body background and text color on scroll using ScrollTrigger, reversing smoothly on scroll-up.
+ * *
  * @build 07.04.26
  * @updated 07.04.26 PHT
  */
@@ -13,30 +15,24 @@ export function initSwitchTheme() {
     const originalBg = bodyStyles.backgroundColor;
     const originalColor = bodyStyles.color;
 
-    ScrollTrigger.create({
-        trigger: section,
-        start: "top 40%",
-        end: "bottom 60%",
-        // markers: true,
-        onEnter: () => {
-            gsap.to("body", {
-                "--background": "#212121",
-                "--text": "#ffffff",
-                color: "#ffffff",
-                backgroundColor: "#212121",
-                duration: 1,
-                ease: "power2.out",
-            });
-        },
-        onLeaveBack: () => {
-            gsap.to("body", {
-                "--background": originalBg,
-                "--text": originalColor,
-                color: originalColor,
-                backgroundColor: originalBg,
-                duration: 1,
-                ease: "power2.out",
-            });
+    gsap.set("body", {
+        "--background": originalBg,
+        "--text": originalColor,
+    });
+
+    gsap.to("body", {
+        "--background": "#212121",
+        "--text": "#ffffff",
+        color: "#ffffff",
+        backgroundColor: "#212121",
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: section,
+            start: "top 40%",
+            end: "bottom 60%",
+            // markers: true,
+            toggleActions: "play reverse play reverse",
         },
     });
 }
