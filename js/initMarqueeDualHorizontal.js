841066dc7d521872d5b9722af7a3d23a57e4d0ce
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-04-08 10:50:48 +0800

    Update initMarqueeDualHorizontal.js

--- js/initMarqueeDualHorizontal.js
+++ js/initMarqueeDualHorizontal.js
@@ -23,6 +23,7 @@ export function initMarqueeDualHorizontal() {
           start: "top bottom",
           end: "bottom top",
           scrub: true,
+          markers: true,
           invalidateOnRefresh: true,
         },
       }
