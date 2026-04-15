fe41db85237b56a303ffd3ad66efa4f60301353a
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-04-15 17:27:07 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ initLanguageRedirect();
 
 import { initHideLoaderOnLoad } from "https://cdn.tonytony.ch/js/initHideLoaderOnLoad.js?v=1.0.0";
 import { initPageTransitions } from "https://cdn.tonytony.ch/js/initPageTransitions.js?v=1.0.0";
-import { initLanguageSwitch } from 'https://cdn.tonytony.ch/js/initLanguageSwitch.js?v=1.0.0';
 import { InitSmoothScrollManager } from "https://cdn.tonytony.ch/js/InitSmoothScrollManager.js?v=1.0.0";
 import { initNavigation } from "https://cdn.tonytony.ch/js/initNavigation.js?v=1.0.0";
 import { initNavBarDisplay } from "https://cdn.tonytony.ch/js/initNavBarDisplay.js?v=1.0.0";
@@ -65,7 +64,6 @@ async function initApp() {
     // 1. CRITICAL / UI LAYER
     // =========================
 
-    // run("initLanguageRedirect", initLanguageRedirect);
     run("HideLoaderOnLoad", initHideLoaderOnLoad);
     run("SmoothScrollManager", () => new InitSmoothScrollManager());
     run("NavBarDisplay", initNavBarDisplay);
